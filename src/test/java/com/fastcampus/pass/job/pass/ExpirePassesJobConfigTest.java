c1f19daffffac3ae0fedaf7d37cf31d310ade7d1
jonghyunhub <chuwhdgus@gmail.com>
2023-12-29 16:47:46 +0900

    test : 테스트 설정 변경

--- src/test/java/com/fastcampus/pass/job/pass/ExpirePassesJobConfigTest.java
+++ src/test/java/com/fastcampus/pass/job/pass/ExpirePassesJobConfigTest.java
@@ -1,11 +1,24 @@
 package com.fastcampus.pass.job.pass;
 
 import com.fastcampus.pass.config.TestBatchConfig;
+import com.fastcampus.pass.repository.pass.PassEntity;
+import com.fastcampus.pass.repository.pass.PassRepository;
+import com.fastcampus.pass.repository.pass.PassStatus;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobInstance;
+import org.springframework.batch.test.JobLauncherTestUtils;
 import org.springframework.batch.test.context.SpringBatchTest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -14,4 +27,46 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBatchTest
 @ActiveProfiles("test")
 @ContextConfiguration(classes = {ExpirePassesJobConfig.class, TestBatchConfig.class})
-class ExpirePassesJobConfigTest {}
+class ExpirePassesJobConfigTest {
+    @Autowired
+    private JobLauncherTestUtils jobLauncherTestUtils; // 이 객체를 통해 프로덕션 코드의 job을 가져와서 테스트 진행
+
+    @Autowired
+    private PassRepository passRepository;
+
+    @Test
+    void test_expirePassStep() throws Exception {
+        // given
+        addPassEntities(10);
+
+        // when
+        JobExecution jobExecution = jobLauncherTestUtils.launchJob();
+        JobInstance jobInstance = jobExecution.getJobInstance();
+
+        // then
+        assertEquals(ExitStatus.COMPLETED, jobExecution.getExitStatus());
+        assertEquals("expirePassesJob", jobInstance.getJobName());
+    }
+
+    private void addPassEntities(int size) {
+        final LocalDateTime now = LocalDateTime.now();
+        Random random = new Random();
+
+        ArrayList<PassEntity> passEntities = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            PassEntity passEntity = PassEntity.builder()
+                    .packageSeq(1)
+                    .userId("A" + 1000000 + i)
+                    .status(PassStatus.IN_PROGRESS)
+                    .remainingCount(random.nextInt(11))
+                    .startedAt(now.minusDays(60))
+                    .endedAt(now.minusDays(1))
+                    .build();
+
+            passEntities.add(passEntity);
+        }
+
+        passRepository.saveAll(passEntities);
+    }
+
+}
