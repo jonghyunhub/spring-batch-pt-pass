75966fe4bb98411c32b3a482a57aa29e08257162
jonghyunhub <chuwhdgus@gmail.com>
2024-01-01 15:10:11 +0900

    feat : 알람 메시지 생성 step 구현

--- src/main/java/com/fastcampus/pass/job/notification/SendNotificationBeforeClassJobConfig.java
+++ src/main/java/com/fastcampus/pass/job/notification/SendNotificationBeforeClassJobConfig.java
@@ -2,10 +2,17 @@ package com.fastcampus.pass.job.notification;
 
 import com.fastcampus.pass.repository.booking.BookingEntity;
 import com.fastcampus.pass.repository.notification.NotificationEntity;
+import com.fastcampus.pass.repository.notification.NotificationEvent;
+import com.fastcampus.pass.repository.notification.NotificationModelMapper;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.database.JpaItemWriter;
+import org.springframework.batch.item.database.JpaPagingItemReader;
+import org.springframework.batch.item.database.builder.JpaItemWriterBuilder;
+import org.springframework.batch.item.database.builder.JpaPagingItemReaderBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -45,6 +52,38 @@ public class SendNotificationBeforeClassJobConfig {
     public Step addNotificationStep() {
         return this.stepBuilderFactory.get("addNotificationStep")
                 .<BookingEntity, NotificationEntity>chunk(CHUNK_SIZE)
+                .reader(addNotificationItemReader())
+                .processor(addNotificationItemProcessor())
+                .writer(addNotificationItemWriter())
+                .build();
+    }
+
+    /**
+     * JpaPagingItemReader : Jpa 에서 사용하는 페이징 기법
+     * 쿼리 당 pageSize 만큼 가져오며 다른 PagingItemReader와 마찬가지로 Thread-safe 함
+     * 이번에 조회 하는 것들은 조회하는 원본 데이터에 대한 업데이트가 이루어지지 않아서 cursor를 사용할 필요가 없음
+     */
+    @Bean
+    public JpaPagingItemReader<BookingEntity> addNotificationItemReader() {
+        // 상태(status)가 준비중이며, 시작일시(startedAt)이 10분 후 시작하는 예약이 알림 대상이 됨
+        return new JpaPagingItemReaderBuilder<BookingEntity>()
+                .name("addNotificationItenReader")
+                .entityManagerFactory(entityManagerFactory)
+                .pageSize(CHUNK_SIZE)
+                .queryString("select b from BookingEntity b join fetch b.userEntity where b.status = :status and b.startedAt <= :startedAt order by b.bookingSeq")
+                .build();
+    }
+
+    @Bean
+    public ItemProcessor<BookingEntity, NotificationEntity> addNotificationItemProcessor() {
+        return bookingEntity -> NotificationModelMapper.INSTANCE.toNotificationEntity(bookingEntity, NotificationEvent.BEFORE_CLASS);
+    }
+
+    @Bean
+    public JpaItemWriter<NotificationEntity> addNotificationItemWriter() {
+        return new JpaItemWriterBuilder<NotificationEntity>()
+                .entityManagerFactory(entityManagerFactory)
+                .build();
     }
 
     /**
--- src/main/java/com/fastcampus/pass/repository/notification/NotificationEntity.java
+++ src/main/java/com/fastcampus/pass/repository/notification/NotificationEntity.java
@@ -6,6 +6,7 @@ import lombok.Setter;
 import lombok.ToString;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
