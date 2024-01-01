45691f5dbf3fedf79571b3731c0249ccade5b3b6
jonghyunhub <chuwhdgus@gmail.com>
2024-01-01 16:14:28 +0900

    feat : 카카오 Message Client 설정 추가

--- src/main/java/com/fastcampus/pass/job/notification/SendNotificationBeforeClassJobConfig.java
+++ src/main/java/com/fastcampus/pass/job/notification/SendNotificationBeforeClassJobConfig.java
@@ -9,14 +9,21 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.database.JpaCursorItemReader;
 import org.springframework.batch.item.database.JpaItemWriter;
 import org.springframework.batch.item.database.JpaPagingItemReader;
+import org.springframework.batch.item.database.builder.JpaCursorItemReaderBuilder;
 import org.springframework.batch.item.database.builder.JpaItemWriterBuilder;
 import org.springframework.batch.item.database.builder.JpaPagingItemReaderBuilder;
+import org.springframework.batch.item.support.SynchronizedItemStreamReader;
+import org.springframework.batch.item.support.builder.SynchronizedItemStreamReaderBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
 
 import javax.persistence.EntityManagerFactory;
+import java.util.Map;
 
 @Configuration
 public class SendNotificationBeforeClassJobConfig {
@@ -91,6 +98,36 @@ public class SendNotificationBeforeClassJobConfig {
      */
     @Bean
     public Step sendNotificationStep() {
+        return this.stepBuilderFactory.get("sendNotificationStep")
+                .<NotificationEntity, NotificationEvent>chunk(CHUNK_SIZE)
+                .reader(sendNotificationItemReader())
+                .writer(sendNotificationItemWriter)
+                .taskExecutor(new SimpleAsyncTaskExecutor())
+                .build();
 
+    }
+
+    /**
+     * 기능 구현을 위해 체크해야 할 포인트 2가지
+     * 1. thread-safe 한가? : paging 조회 기법은 thread-safe 함 / cursor 기법은 thread-safe 하지 않음
+     * 2. 데이터 변경에 무결성 조회가 가능한가? - 가져오는 데이터를 업데이트 했을때 중간에 꼬이는것이 없이 잘 가져올수 있는가?
+     * : cursor 방식은 무결성 조회 가능 / paging 기법은 무결성 조회 불가능 - 데이터 조회시 누락되는 것이 존재할 수 있음
+     * => 따라서 무결성 조회를 위해 cursor 방식을 택하되, cursor 방식은 thread-safe 하지 못하므로 SynchronizedItemStreamReader로 감싸서 Synchronized 하게 처리하면 된다
+     * 이렇게 하게 되면 아이템을 읽는 부분(reader) 어쩔수 없이 순차적으로 실행이 되게 되고(싱글 스레드), 아이템을 쓰는(writer) 경우에는 멀티 스레드로 진행이 됨.
+     * 보통 아이템을 쓰는 부분에 비용이 많이 들기 때문에 유의미한 처리임
+     */
+    @Bean
+    public SynchronizedItemStreamReader<NotificationEntity> sendNotificationItemReader() {
+        // 이벤트(event)가 수업 전이며, 발송 여부(sent)가 미발송인 알람이 조회 대상이 됨
+        JpaCursorItemReader<NotificationEntity> jpaCursorItemReader = new JpaCursorItemReaderBuilder<NotificationEntity>()
+                .name("sendNotificationItemReader")
+                .entityManagerFactory(entityManagerFactory)
+                .queryString("select n from NotificationEntity n where n.event = :event and n.sent = :sent")
+                .parameterValues(Map.of("event",NotificationEvent.BEFORE_CLASS, "sent", false))
+                .build();
+
+        return new SynchronizedItemStreamReaderBuilder<NotificationEntity>()
+                .delegate(jpaCursorItemReader) // delegate : 위임하다
+                .build();
     }
 }
--- src/main/java/com/fastcampus/pass/job/notification/SendNotificationItemWriter.java
+++ src/main/java/com/fastcampus/pass/job/notification/SendNotificationItemWriter.java
@@ -1,4 +1,17 @@
 package com.fastcampus.pass.job.notification;
 
-public class SendNotificationItemWriter {
+import com.fastcampus.pass.repository.notification.NotificationEntity;
+import com.fastcampus.pass.repository.notification.NotificationRepository;
+import org.springframework.batch.item.ItemWriter;
+
+import java.util.List;
+
+public class SendNotificationItemWriter implements ItemWriter<NotificationEntity> {
+
+    private final NotificationRepository notificationRepository;
+    private final
+    @Override
+    public void write(List<? extends NotificationEntity> list) throws Exception {
+
+    }
 }
